6c2f656f75bc3580f210d9c86add33b565ce3bd1
PARK SUNG IL <zihado@gmail.com>
2011-08-26 16:42:36 +0900

    ho

---
+++ FirstApp.java
@@ -0,0 +1,9 @@
+//FirstApp.java 파일에 저장한다
+public class FirstApp {
+  public static void main(String args[]) {
+     System.out.println
+    ("첫번째로 작성한 자바 프로그램");
+     System.out.println
+    ("FirstApp 어플리케이션입니다.");
+  }
+}
